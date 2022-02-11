b305da48e12160b54ca95b5994f4b4f01bec9855
Cesar_a_jr <60815258+cesar-a-jr@users.noreply.github.com>
2022-02-11 08:50:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,47 +4,39 @@ import './App.css';
 export default function App(){
   const[peso, setPeso]= useState('');
   const[altura, setAltura]= useState('');
+  const[telefone, setTelefone]= useState('');
   const[mensagem, setMensagem]= useState('');
-
-  function calcularIMC() {
-    const alt = altura / 100;
-    const imc = peso / (alt * alt);
-
-    if(imc < 18.3) {
-      setMensagem('Você esta abaixo do peso! seu IMC é: '+ imc.toFixed(2) )
-    }else if(imc >= 18.6 && imc < 24.9){
-      setMensagem('Você esta no peso ideal! seu IMC é: '+ imc.toFixed(2) )
-    }else if(imc >=24.9 && imc < 34.9){
-      setMensagem('Você esta levemente acima do peso! seu IMC é: '+ imc.toFixed(2) )   
-    }else if(imc >=34.9){
-      setMensagem('Cuidado OBESIDADE! seu IMC é: '+ imc.toFixed(2) )
-    }
- 
-  }
 
 
   return(
     <div className="app">
-      <h1>Calculadora IMC</h1>
-      <span>Vamos calcular seu IMC</span>
+      <h1>Mudando de Vida!</h1>
+      <span>Se inscreva em nosso curso</span>
 
       <div className="area-input">
         <input 
           type="text"
-          placeholder="peso em (kg) Ex:70"
+          placeholder="Nome"
           value={peso}
           onChange={(e)=> setPeso(e.target.value)}
         />
 
         <input 
           type="text"
-          placeholder="Altura em (cm) Ex:180"
+          placeholder="Email"
           value={altura}
           onChange={(e)=> setAltura(e.target.value)}
         />
 
-        <button onClick={calcularIMC}>
-          Calcular
+        <input 
+          type="text"
+          placeholder="Telefone"
+          value={telefone}
+          onChange={(e)=> setAltura(e.target.value)}
+        />
+
+        <button>
+          Inscrever
         </button>
       </div>
 
